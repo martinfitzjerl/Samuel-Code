68fd05f02ee3b46e3ff33653b6d66030e318f59f
martinfitzjerl <martinfitzjerlsam@yahoo.com>
2017-12-12 09:05:56 -0500

    Update Pizza.cs

--- PizzaStore.Library/Models/Pizza.cs
+++ PizzaStore.Library/Models/Pizza.cs
@@ -4,12 +4,19 @@ namespace PizzaStore.Library.Models
 {
     public class Pizza : Order
     {
-        public void Toppings() {get; set;}
+        public int ToppingCheese {get; set;}
+        public int ToppingSausage { get; set; }
+        public int ToppingPepperoni { get; set;}
+        public int ToppingHam { get; set;}
+        public int ToppingBellPepper { get; set;}
+        public int ToppingChicken { get; set; }
+        public int ToppingSauce { get; set; }
+        public int CrustPan { get; set; }
+        public int CrustDeepDish { get; set; }
+        public int PizzaSize { get; set; }
 
-        public void getToppings()
-        {
-            return Toppings;
-        }
+
+
 
         
 
